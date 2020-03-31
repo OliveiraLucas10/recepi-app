521cb357ecb303e06ec8a093aeb4ba41a5233376
Lucas Oliveira <48152939+OliveiraLucas10@users.noreply.github.com>
2020-03-31 23:04:10 +0100

    Add files via upload

---
+++ src/test/java/com/oliveiralucaspro/recepi/RecepiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveiralucaspro.recepi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
