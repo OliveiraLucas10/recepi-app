b4282be57324c5f9d0e5c51fcd97c6cf133c1032
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-11 10:51:01 +0100

    Query repository

--- src/main/java/com/oliveiralucaspro/recepi/repositories/CategoryRepository.java
+++ src/main/java/com/oliveiralucaspro/recepi/repositories/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.oliveiralucaspro.recepi.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.oliveiralucaspro.recepi.domain.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 
 }
--- src/main/java/com/oliveiralucaspro/recepi/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/oliveiralucaspro/recepi/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package com.oliveiralucaspro.recepi.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.oliveiralucaspro.recepi.domain.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 
 }
