0e554aac22dfc80e79220fd40f4ea1525ca96b88
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-11 14:03:21 +0100

    refactoring

--- src/main/java/com/oliveiralucaspro/recepi/domain/Recipe.java
+++ src/main/java/com/oliveiralucaspro/recepi/domain/Recipe.java
@@ -35,7 +35,7 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
-    
+
     @Lob
     private String directions;
 
@@ -54,5 +54,16 @@ public class Recipe {
     @ManyToMany
     @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recepi_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    public void setNotes(Notes notes) {
+	this.notes = notes;
+	notes.setRecipe(this);
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+	ingredient.setRecipe(this);
+	this.ingredients.add(ingredient);
+	return this;
+    }
 
 }
