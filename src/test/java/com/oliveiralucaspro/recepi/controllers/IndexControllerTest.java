50e60e82bf43b99aa7af2aeb8a5fdf38bfab10a7
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-13 21:17:36 +0100

    added jUnit

--- src/test/java/com/oliveiralucaspro/recepi/controllers/IndexControllerTest.java
+++ src/test/java/com/oliveiralucaspro/recepi/controllers/IndexControllerTest.java
@@ -3,11 +3,16 @@ package com.oliveiralucaspro.recepi.controllers;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.oliveiralucaspro.recepi.services.RecipeService;
@@ -25,6 +30,12 @@ public class IndexControllerTest {
     public void setUp() {
 	MockitoAnnotations.initMocks(this);
 	indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+	MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+	mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
     }
 
     @Test
