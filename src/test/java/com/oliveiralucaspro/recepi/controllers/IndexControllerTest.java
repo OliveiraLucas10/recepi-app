81ed9b1f7f18ce8cec0ec2312482c5a5cf5ac659
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-16 21:11:32 +0100

    test

--- src/test/java/com/oliveiralucaspro/recepi/controllers/IndexControllerTest.java
+++ src/test/java/com/oliveiralucaspro/recepi/controllers/IndexControllerTest.java
@@ -9,14 +9,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.oliveiralucaspro.recepi.services.RecipeService;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class IndexControllerTest {
 
     @Mock
