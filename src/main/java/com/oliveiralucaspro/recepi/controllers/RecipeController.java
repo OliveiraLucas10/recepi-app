961524ac23e2f75b4fe53109264001e4ba535052
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-18 14:45:31 +0100

    added add recipe option

--- src/main/java/com/oliveiralucaspro/recepi/controllers/RecipeController.java
+++ src/main/java/com/oliveiralucaspro/recepi/controllers/RecipeController.java
@@ -2,9 +2,12 @@ package com.oliveiralucaspro.recepi.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.oliveiralucaspro.recepi.commands.RecipeCommand;
 import com.oliveiralucaspro.recepi.services.RecipeService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,21 @@ public class RecipeController {
 	model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
 	return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+	model.addAttribute("recipe", new RecipeCommand());
+
+	return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+	RecipeCommand saveRecipeCommand = recipeService.saveRecipeCommand(command);
+
+	return "redirect:/recipe/show/" + saveRecipeCommand.getId();
     }
 
 }
