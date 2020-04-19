6531a11ae72c7bd6428940db5f7c5c33786e0877
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-19 22:02:48 +0100

    added delete function to ingredients

--- src/main/java/com/oliveiralucaspro/recepi/services/IngredientService.java
+++ src/main/java/com/oliveiralucaspro/recepi/services/IngredientService.java
@@ -3,8 +3,9 @@ package com.oliveiralucaspro.recepi.services;
 import com.oliveiralucaspro.recepi.commands.IngredientCommand;
 
 public interface IngredientService {
-
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
 
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long idToDelete);
 }
