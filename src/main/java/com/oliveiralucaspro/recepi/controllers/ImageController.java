2027baf04372c96813f44ac707fa1d5c9e87231c
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-20 20:04:27 +0100

    UI to upload an image

---
+++ src/main/java/com/oliveiralucaspro/recepi/controllers/ImageController.java
@@ -0,0 +1,37 @@
+package com.oliveiralucaspro.recepi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.oliveiralucaspro.recepi.services.ImageService;
+import com.oliveiralucaspro.recepi.services.RecipeService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class ImageController {
+
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    @GetMapping("recipe/{id}/image")
+    public String showUploadForm(@PathVariable String id, Model model) {
+	model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+	return "recipe/imageuploadform";
+    }
+
+    @PostMapping("recipe/{id}/image")
+    public String handleImagePost(@PathVariable String id, @RequestParam("imagefile") MultipartFile file) {
+
+	imageService.saveImageFile(Long.valueOf(id), file);
+
+	return "redirect:/recipe/" + id + "/show";
+    }
+}
