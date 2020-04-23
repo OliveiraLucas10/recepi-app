3d643f5fef50c07c0cd88b0e6e78342e666f755c
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-04-23 13:42:44 +0100

    handling error page

--- src/main/java/com/oliveiralucaspro/recepi/controllers/RecipeController.java
+++ src/main/java/com/oliveiralucaspro/recepi/controllers/RecipeController.java
@@ -1,13 +1,18 @@
 package com.oliveiralucaspro.recepi.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.oliveiralucaspro.recepi.commands.RecipeCommand;
+import com.oliveiralucaspro.recepi.exceptions.NotFoundException;
 import com.oliveiralucaspro.recepi.services.RecipeService;
 
 import lombok.RequiredArgsConstructor;
@@ -55,5 +60,18 @@ public class RecipeController {
 
 	recipeService.deleteById(Long.valueOf(id));
 	return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+
+	log.error("Handling not found exception");
+
+	ModelAndView modelAndView = new ModelAndView();
+
+	modelAndView.setViewName("404error");
+
+	return modelAndView;
     }
 }
