644c831200b943c0e30b337d1e13a3cc68e3807e
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-05-01 06:12:05 +0100

    added  SQL for the creation of databases, users and to give permission to them

---
+++ scripts/configure-mysql.sql
@@ -0,0 +1,31 @@
+## Use to run mysql db docker image, optional if you're not using a local mysqldb
+# docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+
+# connect to mysql and run as root user
+#Create Databases
+CREATE DATABASE sf_dev;
+CREATE DATABASE sf_prod;
+
+#Create database service accounts
+CREATE USER 'sf_dev_user'@'localhost' IDENTIFIED BY 'oblp';
+CREATE USER 'sf_prod_user'@'localhost' IDENTIFIED BY 'oblp';
+CREATE USER 'sf_dev_user'@'%' IDENTIFIED BY 'oblp';
+CREATE USER 'sf_prod_user'@'%' IDENTIFIED BY 'oblp';
+
+#Database grants
+GRANT SELECT ON sf_dev.* to 'sf_dev_user'@'localhost';
+GRANT INSERT ON sf_dev.* to 'sf_dev_user'@'localhost';
+GRANT DELETE ON sf_dev.* to 'sf_dev_user'@'localhost';
+GRANT UPDATE ON sf_dev.* to 'sf_dev_user'@'localhost';
+GRANT SELECT ON sf_prod.* to 'sf_prod_user'@'localhost';
+GRANT INSERT ON sf_prod.* to 'sf_prod_user'@'localhost';
+GRANT DELETE ON sf_prod.* to 'sf_prod_user'@'localhost';
+GRANT UPDATE ON sf_prod.* to 'sf_prod_user'@'localhost';
+GRANT SELECT ON sf_dev.* to 'sf_dev_user'@'%';
+GRANT INSERT ON sf_dev.* to 'sf_dev_user'@'%';
+GRANT DELETE ON sf_dev.* to 'sf_dev_user'@'%';
+GRANT UPDATE ON sf_dev.* to 'sf_dev_user'@'%';
+GRANT SELECT ON sf_prod.* to 'sf_prod_user'@'%';
+GRANT INSERT ON sf_prod.* to 'sf_prod_user'@'%';
+GRANT DELETE ON sf_prod.* to 'sf_prod_user'@'%';
+GRANT UPDATE ON sf_prod.* to 'sf_prod_user'@'%';
